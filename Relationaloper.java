85cb7beb6140e2720aef338edbf0fea45ca40930
Narean <narean3929@gmail.com>
2026-04-09 19:13:39 +0530

    Create Relationaloper.java

---
+++ Relationaloper.java
@@ -0,0 +1,22 @@
+public class Relationaloperator {
+
+	public static void main(String[] args) {
+		int a=7, b=11;
+		//value of a and b
+		System.out.println("Value of a is" + a +"and b is"+ b);
+		//==operator
+		System.out.println(a==b);//false
+		//!=operator
+		System.out.println(a!=b);//true
+		//a<b
+		System.out.println(a<b);//true
+		//a>b
+		System.out.println(a>b);//false
+		//a<=b
+		System.out.println(a<=b);//true
+		//a>=b
+		System.out.println(a>=b);//false
+
+	}
+
+}
